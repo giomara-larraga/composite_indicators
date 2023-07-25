2793e1147c047a743cfedd7557497b1ccf019e62
Lárraga Maldonado <glarragw@jyu.fi>
2023-07-25 12:46:07 +0300

    functional navigation and code restructuring

---
+++ src/pages/BlankCards.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { AppBar } from "@mui/material";
+
+
+function BlankCards(){
+    return (
+        <div>
+            <AppBar />
+            <p>Inserting blenk cards</p>
+        </div>
+    );
+}
+
+export default BlankCards;
